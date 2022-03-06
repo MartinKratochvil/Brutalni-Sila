23b8160befd67851dde55245155ab560901e5687
Filip Iran <72192710+F1lgas@users.noreply.github.com>
2022-03-06 18:27:12 +0100

    Update Form1.Designer.cs

--- BrutalniSila/Form1.Designer.cs
+++ BrutalniSila/Form1.Designer.cs
@@ -215,7 +215,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = ";";
+            this.Text = "Brutální síla";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
